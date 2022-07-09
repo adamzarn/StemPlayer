6b8a941ed09d6cf730c5af7e7e441fb3be31e040
Adam Zarn <ajzarn@gmail.com>
2022-07-09 10:26:01 -0500

    Initial Commit

---
+++ StemPlayer/StemPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StemPlayerApp.swift
+//  StemPlayer
+//
+//  Created by Adam Zarn on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct StemPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
